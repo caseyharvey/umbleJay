542c2eb36dd63a1b67a63e41ae4ec36aea2f027a
caseyharvey <caseyharvey1@gmail.com>
2020-04-10 13:56:54 +1000

    fixed uppercase bug. add support for ones (unsway)

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Inputs from './Inputs';
-import { styleList } from '../resources/styleList';
-import { formatWords, formatVowel } from '../resources/formats';
-import './App.scss';
+import React from "react";
+import Inputs from "./Inputs";
+import { styleList } from "../resources/styleList";
+import { formatWords, formatVowel, formatOnes } from "../resources/formats";
+import "./App.scss";
 
 class App extends React.Component {
   state = {
-    result: `Welcome to umbleJay, the best pig latin converter in the cosmos. Enter some text, select a style and umbleJay. When random is selected you can continue to hit umbleJay for some crazy combinations`
+    result: `Welcome to umbleJay, the best pig latin converter in the cosmos. Enter some text, select a style and umbleJay. When random is selected you can continue to hit umbleJay for some crazy combinations`,
   };
 
   onTextSubmit = (text, style) => {
-    let last = '';
+    let last = "";
     const random = () => {
       let randomStyle = styleList[Math.floor(Math.random() * styleList.length)];
       if (randomStyle === last) {
@@ -20,21 +20,33 @@ class App extends React.Component {
       return randomStyle;
     };
 
-    const applyStyle = word => {
-      let preStyle = word.split('');
+    //THIS HANDLES THE STRANGE WUNS
+    const styleOnes = (word) => {
+      if (word === "once") {
+        return "unceW" + (style !== "random" ? style : random());
+      }
+      return (
+        word.replace(/^one/, "un") +
+        "W" +
+        (style !== "random" ? style : random())
+      );
+    };
+
+    const applyStyle = (word) => {
+      let preStyle = word.split("");
       // WORDS ARE EVALUATED AND REARRANGED
-      if (preStyle[0] === 'y') {
+      if (preStyle[0] === "y") {
         // WORD STARTS WITH A 'Y'
         return (
-          preStyle.slice(1).join('') +
-          'Y' +
-          (style !== 'random' ? style : random())
+          preStyle.slice(1).join("") +
+          "Y" +
+          (style !== "random" ? style : random())
         );
         //
       } else if (formatVowel.test(preStyle[0])) {
         // WORD STARTS WITH A VOWEL
         return (
-          preStyle.join('') + 'Y' + (style !== 'random' ? style : random())
+          preStyle.join("") + "Y" + (style !== "random" ? style : random())
         );
         //
       } else if (
@@ -43,9 +55,9 @@ class App extends React.Component {
       ) {
         // WORD STARTS WITH A CONSONANT
         return (
-          preStyle.slice(1).join('') +
+          preStyle.slice(1).join("") +
           preStyle[0].toUpperCase() +
-          (style !== 'random' ? style : random())
+          (style !== "random" ? style : random())
         );
         //
       } else if (
@@ -54,30 +66,33 @@ class App extends React.Component {
       ) {
         // WORD STARTS WITH A DIGRAPH
         return (
-          preStyle.slice(2).join('') +
+          preStyle.slice(2).join("") +
           preStyle[0].toUpperCase() +
           preStyle[1] +
-          (style !== 'random' ? style : random())
+          (style !== "random" ? style : random())
         );
       } else {
         // WORD STARTS WITH A TRIGRAPH
         return (
-          preStyle.slice(3).join('') +
+          preStyle.slice(3).join("") +
           preStyle[0].toUpperCase() +
           preStyle[1] +
           preStyle[2] +
-          (style !== 'random' ? style : random())
+          (style !== "random" ? style : random())
         );
       }
     };
     // TEXT IS FORMATTED AND SPLIT INTO AN ARRAY
     const styledArrOfWords = text
+      .toLowerCase()
       .trim()
-      .replace(/\s\s+/g, ' ')
-      .replace(/[^a-zA-Z ]/g, '')
-      .split(' ')
-      .map(word => {
-        if (!formatWords.test(word)) {
+      .replace(/\s\s+/g, " ")
+      .replace(/[^a-zA-Z ]/g, "")
+      .split(" ")
+      .map((word) => {
+        if (formatOnes.test(word)) {
+          return styleOnes(word);
+        } else if (!formatWords.test(word)) {
           return applyStyle(word);
         } else {
           return word;
@@ -85,18 +100,18 @@ class App extends React.Component {
       });
 
     this.setState({
-      result: styledArrOfWords.join(' ')
+      result: styledArrOfWords.join(" "),
     });
   };
 
   render() {
     return (
-      <div className='container'>
-        <div className='header'>
-          <div className='logo'>umbleJay</div>
+      <div className="container">
+        <div className="header">
+          <div className="logo">umbleJay</div>
         </div>
         <Inputs onSubmit={this.onTextSubmit} />
-        <div className='results'>{this.state.result}</div>
+        <div className="results">{this.state.result}</div>
       </div>
     );
   }
